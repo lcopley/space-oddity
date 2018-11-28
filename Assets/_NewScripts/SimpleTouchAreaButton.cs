2b66077a265217707f8f993aba4fc8a76cb182ce
lcopley <lcopley@gmail.com>
2018-11-27 23:15:43 -0800

    Add files via upload

---
+++ Assets/_NewScripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+//add handlers
+
+public class SimpleTouchAreaButton: MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	
+
+	private bool touched;
+	private int pointerId;
+	private bool canFire;
+	
+	void Awake(){
+		touched = false;
+	}
+	// Set Start Point
+	public  void OnPointerDown(PointerEventData data) {
+		if (!touched) {
+			touched = true;
+			pointerId = data.pointerId;
+			canFire= true;
+		}
+	}
+
+	public void  OnPointerUp(PointerEventData data) {
+		if (data.pointerId==pointerId){
+			canFire = false;
+			touched = false;
+
+		}
+	}
+	public bool CanFire(){
+		return canFire;
+	}
+	
+}
